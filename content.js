de364967e25dc7f256254e36fbe0a230653e871b
danieI <daniellysack@gmail.com>
2017-05-24 01:37:25 -0500

    No longer require halt at window close.
    All chrome storage use happens when window loads, and only processing of grade value occurs on submit button click.

--- content.js
+++ content.js
@@ -1,41 +1,71 @@
-// var partialNumbers = {}
+var students = {}
 
-// function loadIncomplete(incompleteName){
-//   chrome.storage.sync.get(incompleteName, function(obj){
-//     partialNumbers[incompleteName] = obj[incompleteName] || requestNumber(incompleteName);
-//   });
-// }
-
-// function requestNumber(incomplete) {
-// 	var complete = prompt("Enter complete student number for " + incomplete);
-//   var data = {[incomplete]: complete};
-//   chrome.storage.sync.set(data, function() {
-//   console.log("Linked '" + incomplete + "' to '" + complete + "'.");
-// });
-// 	return complete;
-// }
-
-function sendGrades() {
-  var students = {};
-  var studentCount = body.getElementById('StudentCount').value;
-  var outOf = body.getElementById('OutOf').value;
-
-  var i; for (i = 0; i < studentCount; i++) {
-    var workingRow = body.getElementsByTagName('tr').item(i);
-    var workingId = workingRow.getElementsByTagName('td')[0].innerHTML;
-
-    var matches = /\(([^)]+)\)/.exec(workingId); // remove enclosing brackets
-    workingId = (matches != null) ? matches[1] : "10048"; 
-
-    var workingGrade = workingRow.getElementsByTagName('input')[1].value;
-    workingGrade = Math.round(workingGrade / outOf * 100).toString();
-    students[workingId] = workingGrade;
+function setId(obj, incomplete) {
+if (obj[incomplete] === undefined) {
+  id = prompt("Enter comlete student id for " + incomplete);
+  chrome.storage.sync.set({[incomplete]: id}, function (){
+    console.log("success");
+  });
+  return(id);
+}
+else {
+  return(obj[incomplete]);
   }
-  chrome.runtime.sendMessage({data: students});
 }
 
-body = $('[name="GradeTaskBody"]')[0].contentWindow.document;
-footer = $('[name="GradeTaskBottom"]')[0].contentWindow.document;
+function getGrades(ids, td){
+  var grade = "";
+   for (i = 0; i < td.length; i++){
+     if (tr[i].getElementsByTagName("td")[3].getElementsByTagName("select")[0].options[0].selected) {
+      grade = Math.round(td[i].getElementsByTagName("input")[0].value / document.getElementById("OutOf").value * 100).toString();
+      students[ids[i]] = grade;
+   }
+   else {
+    // grade = "Grade Not Entered" *NR selected, or other excused codes or grade = "assignment not completed"
+   }
+     //students[ids[i]] = grade;
+   }
+ }
+ 
 
-footer.getElementById("Submit1").addEventListener("click", function () {
-  sendGrades(); });
+ function run() {
+   alert("Complete");
+
+    var body = document.getElementById("GradeTaskBody").contentWindow.document;
+    var footer = document.getElementById("GradeTaskBottom").contentWindow.document;
+var body = document;
+    var  tr = body.getElementsByTagName("table")[0].getElementsByTagName("tr");
+    var td = [];
+    var ids = [];
+    for (i = 0; i < tr.length; i++){
+      td.push(tr[i].getElementsByTagName("td")[4]);
+      var info = /\(([^)]+)\)/.exec(tr[i].getElementsByTagName("td")[0].innerHTML);
+      var id = "";
+      if (info === null){
+        var incomplete = tr[i].getElementsByTagName("td")[0].innerHTML.trim();
+        chrome.storage.sync.get(incomplete, function(obj){
+          ids.push(setId(obj, incomplete));
+        });
+      }
+          else {
+            id = info[1];
+            ids.push(id);
+          }
+      }
+
+    footer.getElementById("Submit1").addEventListener("click", function () {
+      getGrades(ids, td);
+      chrome.runtime.sendMessage({data: students});
+    });
+ }
+
+ 
+
+ if (document.readyState === "complete") {
+   run();
+}
+document.onreadystatechange = function () {
+  if (document.readyState === "complete") {
+    run();
+}
+}
