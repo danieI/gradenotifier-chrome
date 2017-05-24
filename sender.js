42b4006b055f13b3c6915dab38431613c311617c
Ianden <landendanyluk@gmail.com>
2017-05-23 19:09:48 -0500

    Added check for sender.js

--- sender.js
+++ sender.js
@@ -1,5 +1,6 @@
 chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse) {
+		console.log(request.data);
 		$.post("https://gradenotifier.com/sms/send", 
 			{ data: JSON.stringify(request.data) }
 		);	
